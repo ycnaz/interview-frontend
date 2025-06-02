cf35343499618325610e2b1b2776cea0ea33521b
Yasin <yasinisworking@gmail.com>
2025-06-02 14:54:35 +0330

    Responsiveness of product page

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,101 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import { useProductStore } from './stores/productStore';
 
 const productStore = useProductStore()
 
+const burgerOpen = ref(false)
+
+const toggleHamburger = () => {
+  burgerOpen.value = !burgerOpen.value
+}
+
+const closeBurger = () => {
+  burgerOpen.value = false
+}
+
 onMounted(() => productStore.fetchProducts())
 </script>
 
 <template>
-  <header class="w-full h-[120px] bg-white rounded-b-4xl">
-    <nav class="grid grid-cols-3 text-thirteen h-full">
-      <div></div>
-      <div class="flex justify-self-center self-center gap-x-10">
-        <div class="flex gap-x-2 min-w-max">
+  <header class="w-full h-[120px] bg-white rounded-b-4xl max-xs:h-[72px]">
+    <nav
+      class="xs:grid xs:grid-cols-3 text-thirteen h-full max-xs:px-4 max-xs:flex max-xs:justify-between max-xs:items-center max-xs:relative">
+
+      <div class="max-xs:hidden"></div>
+
+      <div
+        :class="['flex justify-self-center self-center gap-x-10 max-xs:absolute max-xs:top-5 max-xs:z-10 max-xs:bg-accent-og max-xs:h-dvh max-xs:w-full max-xs:pt-20 max-xs:text-2xl max-xs:gap-y-5 max-xs:flex-col max-xs:items-center transition-all', burgerOpen ? 'max-xs:right-0' : 'max-xs:-right-96']">
+
+        <div @click="closeBurger" class="flex gap-x-2 min-w-max">
           <img loading="lazy" alt="Nav link SVGs" src="./assets/svg/navbar-category-2.svg">
-          <RouterLink to="#">لیست محصولات</RouterLink>
+          <RouterLink :to="{ name: 'products' }">لیست محصولات</RouterLink>
         </div>
-        <div class="flex gap-x-2 min-w-max">
+
+        <div @click="closeBurger" class="flex gap-x-2 min-w-max">
           <img loading="lazy" alt="Nav link SVGs" src="./assets/svg/navbar-book.svg">
           <RouterLink to="#">دریافت مشاوره</RouterLink>
         </div>
-        <div class="flex gap-x-2 min-w-max">
+
+        <div @click="closeBurger" class="flex gap-x-2 min-w-max">
           <img loading="lazy" alt="Nav link SVGs" src="./assets/svg/navbar-message-question.svg">
           <RouterLink to="#">سوالات متداول</RouterLink>
         </div>
-        <div class="relative flex gap-x-2 text-accent-og min-w-max">
+
+        <div @click="closeBurger"
+          class="relative flex gap-x-2 text-accent-og min-w-max max-xs:bg-white max-xs:px-4 max-xs:py-3 max-xs:rounded-xl">
           <img loading="lazy" alt="Nav link SVGs" src="./assets/svg/navbar-fi_phone-call.svg">
           <RouterLink to="#"
             class="after:absolute after:size-1 after:rounded-full after:bg-accent-og after:-bottom-3 after:left-1/2">
             تماس
             با ما</RouterLink>
         </div>
+
       </div>
-      <div class="justify-self-center self-center">
+
+      <div class="justify-self-center self-center max-xs:hidden">
         <button
           class="flex gap-x-3 justify-center text-fourteen rounded-2xl bg-accent-og py-3 px-4 w-[107px] text-white">
           <span>تماس</span>
           <img loading="lazy" alt="Navbar Phone SVG" src="./assets/svg/navbar-phone.svg">
         </button>
       </div>
+
+      <div @click="toggleHamburger"
+        :class="['flex justify-center items-center size-10 rounded-lg border border-accent-og cursor-pointer xs:hidden', { 'fixed top-4 -right-4 z-20 border-white': burgerOpen }]">
+        <div class="flex flex-col items-center justify-center gap-y-[3px] size-4">
+          <div
+            :class="['w-full h-px rounded-lg bg-accent-og transition-all', burgerOpen ? 'rotate-45 translate-y-0.5 bg-white' : '']">
+          </div>
+          <div :class="['w-full h-px rounded-lg bg-accent-og transition-all', burgerOpen ? 'hidden bg-white' : '']">
+          </div>
+          <div
+            :class="['w-full h-px rounded-lg bg-accent-og transition-all', burgerOpen ? '-rotate-45 -translate-y-0.5 bg-white' : '']">
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center size-10 rounded-lg border border-accent-og mr-auto xs:hidden">
+        <img loading="lazy" alt="Navbar Phone SVG" src="./assets/svg/xs-navbar-phone.svg">
+      </div>
     </nav>
   </header>
 
-  <main class="mx-auto mt-6 max-w-[1112px] max-[1028px]:max-w-[868px] min-h-dvh">
+  <main class="mx-auto mt-6 max-w-[1112px] max-[1028px]:max-w-[868px] max-xs:w-11/12 min-h-dvh">
     <RouterView />
   </main>
 
   <footer
-    class="relative bottom-0 left-0 flex flex-col rounded-t-2xl shadow-sm bg-white w-full h-[416px] mt-6 leading-6">
-    <div class="grid grid-cols-3 w-full h-full">
-      <div class="self-end justify-self-center pb-14">
+    class="relative bottom-0 left-0 flex flex-col rounded-t-2xl shadow-sm bg-white w-full h-[416px] mt-6 leading-6 max-xs:h-[846px]">
+    <div class="grid grid-cols-3 w-full h-full max-xs:grid-cols-2 max-xs:px-6 max-xs:gap-x-4">
+      <div class="self-end justify-self-center pb-14 max-xs:hidden">
         <span class="text-thirteen leading-6 text-text-gray">هفت روز هفته از ۸ صبح تا ۱۲ شب پاسخگو
           هستیم</span>
       </div>
 
-      <div class="col-span-2 grid grid-cols-3 mt-10">
+      <div
+        class="col-span-2 grid grid-cols-3 mt-10 max-xs:flex max-xs:flex-wrap max-xs:mt-40 max-xs:mb-10 max-xs:justify-between">
         <div class="flex flex-col gap-y-6">
           <h6 class="text-footer-h-text text-xl">دسترسی سریع</h6>
           <div class="flex flex-col gap-y-3 text-footer-s-text text-thirteen">
@@ -93,8 +133,9 @@ onMounted(() => productStore.fetchProducts())
       </div>
     </div>
 
-    <div class="grid grid-cols-3 items-center h-[152px] w-full shrink-0 bg-footer mt-auto">
-      <div></div>
+    <div
+      class="grid grid-cols-3 items-center h-[152px] w-full shrink-0 bg-footer mt-auto max-xs:flex max-xs:flex-col max-xs:h-[243px] max-xs:gap-y-4 max-xs:justify-center max-xs:px-3">
+      <div class="max-xs:hidden"></div>
 
       <div class="flex flex-col items-center gap-y-4">
         <p class="text-thirteen text-text-gray text-center">تمامی حقوق مادی و معنوی این وبسایت متعلق به می‌باشد و هر
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -7,6 +7,11 @@ const productStore = useProductStore()
 const route = useRoute()
 
 const product = ref(null)
+const isImgLoading = ref(true)
+
+const imgLoaded = () => {
+    isImgLoading.value = false
+}
 
 onMounted(async () => {
     await productStore.fetchProducts()
@@ -16,7 +21,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div v-if="product" class="flex flex-col gap-y-6">
+    <div v-if="product" class="flex flex-col w-full gap-y-6">
         <div class="breadcrumbs text-fourteen text-breadcrumb">
             <ul>
                 <li><img loading="lazy" alt="Breadcrumb Home SVG" src="../assets/svg/home.svg"></li>
@@ -26,10 +31,13 @@ onMounted(async () => {
                 <li><span>{{ product.title }}</span></li>
             </ul>
         </div>
-        <div class="flex flex-col justify-between h-[414px] w-full bg-white rounded-3xl shadow-sm p-6">
+        <div class="flex flex-col justify-between h-[414px] w-full bg-white rounded-3xl shadow-sm p-6 max-xs:h-[331px]">
             <h2 class="leading-6 text-lg text-text">{{ product.title }}</h2>
-            <div class="relative w-full h-[310px] rounded-2xl">
-                <img loading="lazy" alt="Product Image" :src="product.image" class="object-contain w-full h-full">
+            <div class="flex justify-center items-center relative w-full h-[310px] rounded-2xl max-xs:h-[235px]">
+                <img loading="lazy" alt="Product Image" :src="product.image"
+                    :class="['object-contain w-full h-full', isImgLoading ? 'scale-0 absolute' : 'scale-100']"
+                    @load="imgLoaded">
+                <span v-if="isImgLoading" class="loading loading-spinner text-accent-og"></span>
                 <div
                     class="absolute top-0 right-0 flex items-center justify-center rounded-[13px] bg-black opacity-50 size-10">
                     <img loading="lazy" alt="Zoom In Button" src="../assets/svg/search-zoom-in.svg" class="">
@@ -37,43 +45,70 @@ onMounted(async () => {
             </div>
         </div>
 
-        <div class="flex flex-col justify-between h-[474px] w-full bg-white rounded-3xl shadow-sm p-6">
+        <div class="flex flex-col justify-between h-[474px] w-full bg-white rounded-3xl shadow-sm p-6 max-xs:h-auto">
             <h2 class="text-text font-bold leading-6">مشخصات فنی</h2>
-            <div class="flex gap-x-3 h-full max-h-[386px] w-full">
-                <div class="flex flex-col w-[172px] h-full shrink-0 gap-y-3 text-fourteen text-product-page-label">
-                    <div class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center">
-                        <span>قیمت</span>
-                    </div>
-                    <div class="bg-inner-bg rounded-r-2xl rounded-l-sm flex grow min-h-12 w-full items-center pr-4">
-                        <span>توضیحات</span>
-                    </div>
-                    <div class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center">
-                        <span>دسته بندی</span>
-                    </div>
-                    <div class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center">
-                        <span>رتبه</span>
-                    </div>
-                    <div class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center">
-                        <span>تعداد</span>
-                    </div>
-                </div>
+            <div class="flex gap-x-3 h-full max-h-[386px] w-full max-xs:max-h-min">
+                <div class="flex flex-col w-full h-full gap-y-3 text-fourteen text-product-page-label">
+                    <div class="flex xs:gap-x-3 max-xs:flex-col max-xs:gap-y-3">
+                        <div
+                            class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center xs:max-w-[172px] max-xs:rounded-2xl">
+                            <span>قیمت</span>
+                        </div>
 
-                <div class="flex flex-col gap-y-3 grow">
-                    <div class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex pr-4 items-center">
-                        <span>{{ product.price }}$</span>
+                        <div
+                            class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex px-4 items-center max-xs:rounded-2xl">
+                            <span>{{ product.price }}$</span>
+                        </div>
                     </div>
-                    <div class="bg-inner-bg rounded-l-2xl rounded-r-sm flex grow min-h-12 w-full items-center pr-4">
-                        <span>{{ product.description }}</span>
+
+                    <div class="flex xs:gap-x-3 xs:min-h-[146px] max-xs:flex-col max-xs:gap-y-3">
+                        <div
+                            class="bg-inner-bg rounded-r-2xl rounded-l-sm flex grow min-h-12 w-full items-center xs:max-w-[172px] pr-4 max-xs:rounded-2xl">
+                            <span>توضیحات</span>
+                        </div>
+
+                        <div
+                            class="bg-inner-bg rounded-l-2xl rounded-r-sm flex grow min-h-12 w-full items-center p-4 max-xs:rounded-2xl">
+                            <span>{{ product.description }}</span>
+                        </div>
                     </div>
-                    <div class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex pr-4 items-center">
-                        <span>{{ product.category }}</span>
+
+                    <div class="flex xs:gap-x-3 max-xs:flex-col max-xs:gap-y-3">
+                        <div
+                            class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center xs:max-w-[172px] max-xs:rounded-2xl">
+                            <span>دسته بندی</span>
+                        </div>
+
+                        <div
+                            class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex px-4 items-center max-xs:rounded-2xl">
+                            <span>{{ product.category }}</span>
+                        </div>
                     </div>
-                    <div class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex pr-4 items-center">
-                        <span>-</span>
+
+                    <div class="flex xs:gap-x-3 max-xs:flex-col max-xs:gap-y-3">
+                        <div
+                            class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center xs:max-w-[172px] max-xs:rounded-2xl">
+                            <span>رتبه</span>
+                        </div>
+
+                        <div
+                            class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex px-4 items-center max-xs:rounded-2xl">
+                            <span>-</span>
+                        </div>
                     </div>
-                    <div class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex pr-4 items-center">
-                        <span>-</span>
+
+                    <div class="flex xs:gap-x-3 max-xs:flex-col max-xs:gap-y-3">
+                        <div
+                            class="bg-inner-bg rounded-r-2xl rounded-l-sm h-12 w-full flex pr-4 items-center xs:max-w-[172px] max-xs:rounded-2xl">
+                            <span>تعداد</span>
+                        </div>
+
+                        <div
+                            class="bg-inner-bg rounded-l-2xl rounded-r-sm h-12 w-full flex px-4 items-center max-xs:rounded-2xl">
+                            <span>-</span>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
